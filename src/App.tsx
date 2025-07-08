33f77efc588eb81506506769be04448c9953aa12
zenheart <zenheart_register@163.com>
2025-07-08 12:51:53 +0800

    fix: 修改路由使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { HashRouter as Router } from 'react-router';
-import { TREE_COMPONENTS, TAGS_COLOR } from './const'
-import Nav from './components/Nav';
+import { TREE_COMPONENTS, TAGS_COLOR } from "./const";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+      <div className="App">
         {/* @ts-ignore */}
         <Nav children={TREE_COMPONENTS} tagsColor={TAGS_COLOR}></Nav>
-      </Router>
-    </div>
-  )
+      </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './polyfill'
+import { HashRouter } from "react-router";
+
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
