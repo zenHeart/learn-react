be0b89dd475b8bcfffbb65a945912107a042cc6d
zenheart <zenheart_register@163.com>
2025-07-16 09:03:55 +0800

    deploy

---
+++ assets/HelloWorld-CdGz_qzq.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CWg4_TOG.js";function l(){return o.jsx("h1",{children:"hello world"})}export{l as default};
--- assets/HelloWorld-LGr_Kzd2.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-ROh2F-Qe.js";function l(){return o.jsx("h1",{children:"hello world"})}export{l as default};
